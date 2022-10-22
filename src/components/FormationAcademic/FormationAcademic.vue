2193377a9c6ab6180bb7de1bfb32c4e23938ab46
alexandretavanocardoso <AlexandreTavanoDeveloper@outlook.com>
2022-10-22 19:58:33 -0300

    :tada: Update formation academic

--- src/components/FormationAcademic/FormationAcademic.vue
+++ src/components/FormationAcademic/FormationAcademic.vue
@@ -12,32 +12,56 @@
                 <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne"
                     data-bs-parent="#accordionFlushExample">
                     <div class="accordion-body">
-                        Módulo I <br />
-                        1. Linguagem, Trabalho e Tecnologia <br />
-                        2. Programação e Algoritmos <br />
-                        3. Banco de Dados I <br />
-                        4. Análise e Projeto de Sistemas <br />
-                        5. Design Digital <br />
-                        6. Programação Web I <br />
-                        7. Fundamentos da Informática <br />
-                        8. Técnicas de Programação <br /><br />
-                        Módulo II <br />
-                        1. Inglês Instrumental <br />
-                        2. Desenvolvimento de Sistemas <br />
-                        3. Banco de Dados II <br />
-                        4. Internet e Protocolos <br />
-                        5. Programação de Aplicativos Mobile I <br />
-                        6. Programação Web II <br />
-                        7. Planejamento do Trabalho de Conclusão de Curso (TCC) <br /><br />
-                        Módulo III <br />
-                        1. Segurança de Sistemas de Informação <br />
-                        2. Banco de Dados III <br />
-                        3. Sistemas Embarcados <br />
-                        4. Programação de Aplicativos Mobile II <br />
-                        5. Programação Web III <br />
-                        6. Qualidade e Teste de Software <br />
-                        7. Ética e Cidadania Organizacional <br />
-                        8. Desenvolvimento do Trabalho de Conclusão de Curso (TCC)
+                        <div>
+                            <h5>Descrição</h5>
+                            <p>É o profissional que analisa e projeta sistemas. Constrói, documenta, realiza testes e
+                                mantém sistemas de informação. Utiliza ambientes de desenvolvimento e linguagens de
+                                programação específica. Modela, implementa e mantém bancos de dados.</p>
+                        </div>
+                        <hr />
+                        <div>
+                            <h5>Grade curricular</h5>
+                            <div style="margin-top: 20px;" class="row">
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>Módulo I</b></li>
+                                        <li>Linguagem, Trabalho e Tecnologia.</li>
+                                        <li>Programação e Algoritmos.</li>
+                                        <li>Banco de Dados I.</li>
+                                        <li>Análise e Projeto de Sistemas.</li>
+                                        <li>Design Digital.</li>
+                                        <li>Programação Web I.</li>
+                                        <li>Fundamentos da Informática.</li>
+                                        <li>Técnicas de Programação.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>Módulo II</b></li>
+                                        <li>Inglês Instrumental.</li>
+                                        <li>Desenvolvimento de Sistemas.</li>
+                                        <li>Banco de Dados II.</li>
+                                        <li>Internet e Protocolos.</li>
+                                        <li>Programação de Aplicativos Mobile I.</li>
+                                        <li>Programação Web II.</li>
+                                        <li>Planejamento do Trabalho de Conclusão de Curso (TCC).</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>Módulo III</b></li>
+                                        <li>Segurança de Sistemas de Informação.</li>
+                                        <li>Banco de Dados III.</li>
+                                        <li>Sistemas Embarcados.</li>
+                                        <li>Programação de Aplicativos Mobile II.</li>
+                                        <li>Programação Web III.</li>
+                                        <li>Qualidade e Teste de Software.</li>
+                                        <li>Ética e Cidadania Organizacional.</li>
+                                        <li>Desenvolvimento do Trabalho de Conclusão de Curso (TCC).</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -45,15 +69,121 @@
                 <h2 class="accordion-header" id="flush-headingTwo">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                        Faculdade FGP - Sistema de Informação
+                        Faculdade FGP - Sistema de Informação (Cursando)
                     </button>
                 </h2>
                 <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo"
                     data-bs-parent="#accordionFlushExample">
                     <div class="accordion-body">
-                        Este curso tem como objetivo, formar profissionais que, apoiados nos conceitos e técnicas de
-                        informática, teoria de sistemas e administração, contribuam na solução de problemas de
-                        tratamento de informação nas organizações, por meio da concepção.
+                        <div>
+                            <h5>Descrição</h5>
+                            <p>Este curso tem como objetivo, formar profissionais que, apoiados nos conceitos e técnicas
+                                de informática, teoria de sistemas e administração, contribuam na solução de problemas
+                                de tratamento de informação nas organizações, por meio da concepção, construção e
+                                manutenção de modelos informatizados de automação corporativa.</p>
+                        </div>
+                        <hr />
+                        <div>
+                            <h5>Grade curricular</h5>
+                            <div style="margin-top: 20px;" class="row">
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>1º Semestre: Núcleo Instrumental</b></li>
+                                        <li>Empresas, Mercados e Negócios.</li>
+                                        <li>Matemática Aplicada.</li>
+                                        <li>Comunicação Empresarial.</li>
+                                        <li>Psicologia Aplicada.</li>
+                                        <li>Fundamentos da Administração.</li>
+                                        <li>Projeto Integrador.</li>
+                                        <li>Atividades Complementares.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>2º Semestre: Núcleo Instrumental</b></li>
+                                        <li>Algoritmos e Lógica de Programação.</li>
+                                        <li>Estatística Aplicada.</li>
+                                        <li>Desenvolvimento Humano e Profissional.</li>
+                                        <li>Fundamentos da WEB.</li>
+                                        <li>Filosofia e Ética profissional.</li>
+                                        <li>Projeto Integrador.</li>
+                                        <li>Atividades Complementares.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>3º Semestre: Núcleo Instrumental</b></li>
+                                        <li>Fundamentos e Estrutura de Banco de Dados.</li>
+                                        <li>Sistemas de Informações Gerenciais Integrados.</li>
+                                        <li>Engenharia de Software.</li>
+                                        <li>Arquitetura e Organização de Computadores.</li>
+                                        <li>Sociologia e Relações Étnico Raciais.</li>
+                                        <li>Projeto Integrador.</li>
+                                        <li>Atividades Complementares.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>4º Semestre: Núcleo Instrumental</b></li>
+                                        <li>Introdução a Orientação por Objetos.</li>
+                                        <li>Sistemas Operacionais.</li>
+                                        <li>Tópicos em Engenharia de Software.</li>
+                                        <li>Tópicos em Estrutura de Dados.</li>
+                                        <li>Fundamentos de Redes de Computadores.</li>
+                                        <li>Projeto Integrador.</li>
+                                        <li>Atividades Complementares.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>5º Semestre: Núcleo Instrumental</b></li>
+                                        <li>Gerenciamento e Monitoramento de Redes de Computadores.</li>
+                                        <li>Gerenciamento e Segurança de Banco de Dados.</li>
+                                        <li>Desenvolvimento de Software para WEB e Dispositivos Móveis.</li>
+                                        <li>Governança Corporativa Empreendedorismo.</li>
+                                        <li>Projeto Integrador.</li>
+                                        <li>Atividades Complementares.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>6º Semestre: Núcleo Instrumental</b></li>
+                                        <li>Computação Cognitiva e Semântica.</li>
+                                        <li>Business Intelligence e Big Data Analycts.</li>
+                                        <li>Design para WEB.</li>
+                                        <li>Gestão de Projetos Marketing Digital.</li>
+                                        <li>Projeto Integrador.</li>
+                                        <li>Atividades Complementares.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>7º Semestre: Núcleo Instrumental</b></li>
+                                        <li>Integração Entre Sistemas Computacionais.</li>
+                                        <li>Trabalho Conclusão de Curso I.</li>
+                                        <li>Orientação Estagio Supervisionado.</li>
+                                        <li>Gestão Ambiental.</li>
+                                        <li>Negociação e Resolução de Conflitos.</li>
+                                        <li>Estagio Supervisionado.</li>
+                                        <li>Projeto Integrador.</li>
+                                        <li>Atividades Complementares.</li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-4">
+                                    <ul>
+                                        <li><b>8º Semestre: Núcleo Instrumental</b></li>
+                                        <li>Arquitetura Orientada a Serviço e WEBSERVICE.</li>
+                                        <li>Administração de Recursos Humanos.</li>
+                                        <li>Inovação e Negócios.</li>
+                                        <li>Trabalho de Conclusão de Curso II.</li>
+                                        <li>Orientação Estagio Supervisionado.</li>
+                                        <li>Estagio Supervisionado.</li>
+                                        <li>Projeto Integrador.</li>
+                                        <li>Atividades Complementares.</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -66,6 +196,30 @@ export default {
     name: 'FormationAcademicComponent'
 }
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
